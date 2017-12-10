4df288ddf7c8232891acfb0abe9241039019b062
angpao <angpao.kittimasak@gmail.com>
2017-12-10 14:28:32 +0700

    change angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  private result: Array<any>;
+
+  constructor(private dataService: DataService) {
+    this.onLoad();
+  }
+
+  onLoad() {
+    this.dataService.getCourse().subscribe(res => {
+      this.result = res;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { DataService } from './data.service';
+
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +14,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
