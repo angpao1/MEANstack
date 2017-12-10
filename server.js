454573ceebc51ecd13a0293f707212b476f886f7
angpao <angpao.kittimasak@gmail.com>
2017-12-10 13:20:14 +0700

    add express add router

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/api', require('./server/api'));
 
-app.use('*', function(req, res) {
+
+app.use('/api', require('./server/api.js'));
+
+app.use('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-const server = app.listen(8081, function() {
+const server = app.listen(8081, function () {
   const port = server.address().port;
-  console.log('server is running at port: %s', port);
+  console.log('Server is running... at port: %s', port)
 })
--- server/api.js
+++ server/api.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/show', function(req, res) {
-  res.end('Hi');
+router.get('/show', function (req, res) {
+  res.end('Hi, show api');
 })
 
-router.post('/add', function(req, res) {
-  res.end('add api' + req.body.name);
+router.post('/add', function (req, res) {
+  res.end('Hi, add api: ' + req.body.name);
 })
 
-router.delete('/delete/:name', function(req, res) {
-  res.end('Hi, Delete api' + req.params.name);
+router.delete('/delete/:name', function (req, res) {
+  res.end('Hi, delete api: ' + req.params.name);
 })
-
 
 module.exports = router;
-
