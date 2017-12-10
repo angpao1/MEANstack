360718c0103904b33231e1a326b69fa7f8cff8ac
angpao <angpao.kittimasak@gmail.com>
2017-12-10 14:57:14 +0700

    finish project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,31 @@ import { DataService } from './data.service';
 export class AppComponent {
 
   private result: Array<any>;
+  private name: string;
 
   constructor(private dataService: DataService) {
     this.onLoad();
   }
 
+  onAdd() {
+    this.dataService.addCourse(this.name).subscribe(res => {
+      console.log(res);
+      this.onLoad();
+    });
+  }
+
   onLoad() {
     this.dataService.getCourse().subscribe(res => {
+      console.log(res);
       this.result = res;
+    });
+  }
+
+  onDelete(name: string) {
+    this.dataService.deleteCourse(name).subscribe(res => {
+      console.log(res);
+      this.result = res;
+      this.onLoad();
     });
   }
 }
